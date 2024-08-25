ee241a3f8b8717fb62a0effe170b35762023af4d
Bourrelet <eva.boulanger4@gmail.com>
2024-08-25 18:49:53 +0200

    Banner Done; Home wip

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -4,6 +4,7 @@ import './Banner.scss';
 function Banner() {
     return (
         <div className="banner">
+            <img className="banner__img" src="/shore.png" alt="moutains background"/>
             <div className="banner__overlay"></div>
             <h1 className="banner__title">Chez vous, partout et ailleurs</h1>
         </div>
