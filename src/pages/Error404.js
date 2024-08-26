f64d8e2354d018ee7f63005d858ec46c53b296e3
Bourrelet <eva.boulanger4@gmail.com>
2024-08-26 20:56:42 +0200

    404 Done

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -6,9 +6,10 @@ import './Error404.scss';
 const Error404 = () => {
   return (
     <div className="error">
-      <h1 className="error__title1">404</h1>
-      <h2 className="error__title2">This is the Error404 Page.</h2>
-      <Link className="error__link" to="/">Retourner sur la page d’accueil</Link>
+      <h1 className="error__title1 error__margin">404</h1>
+      <h2 className="error__title2 error__margin">Oups! La page que<br/>
+      vous demandez n'existe pas.</h2>
+      <Link className="error__link error__margin" to="/">Retourner sur la page d’accueil</Link>
     </div>
   );
 }
