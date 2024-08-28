131a68a1e745ef221ca1667f11c550659e216b69
Bourrelet <eva.boulanger4@gmail.com>
2024-08-28 21:23:56 +0200

    Structure FicheLogement Done ; Components WIP

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Card.scss';
+import logement from '../../data/logement.json';
+
+
 
 const Card = () => {
     return (
     <div className="card-frame">
+        
         <strong className="card-frame__title">
             Titre de la <br/> location
         </strong>
-    
-        
     </div>
     );
 
