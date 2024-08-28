8047796ff20d78d1dd73ac821979d4453725a0d6
Bourrelet <eva.boulanger4@gmail.com>
2024-08-28 16:14:26 +0200

    Apparence Collapse Done

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Collapse.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+const openButton = <FontAwesomeIcon icon={faAngleUp} />;
+const closeButton = <FontAwesomeIcon icon={faAngleDown} />;
 
 const Collapse = () => {
     return (
     <div className="collapse">
         <div className="collapse__container">
-            <h2 className="collapse__title">Titre du collapse</h2>
-            <p className="collapse__text">Text du collapse</p>  
+            <h2 className="collapse__title">Titre du collapse</h2>   
+
+                <button className="collapse__btn open-btn">{openButton}</button>
+         
         </div>
+        <p className="collapse__text">Text du collapse</p> 
     </div>
     );
 
