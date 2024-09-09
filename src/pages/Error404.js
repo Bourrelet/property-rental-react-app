5437a53faf31890b3ba5caf11a6f1eca229b6cd7
Bourrelet <eva.boulanger4@gmail.com>
2024-09-09 22:48:09 +0200

    Responsive WIP ; Page Logement a faire

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -7,7 +7,7 @@ const Error404 = () => {
   return (
     <div className="error">
       <h1 className="error__title1 error__margin">404</h1>
-      <h2 className="error__title2 error__margin">Oups! La page que<br/>
+      <h2 className="error__title2 error__margin">Oups! La page que <br/>
       vous demandez n'existe pas.</h2>
       <Link className="error__link error__margin" to="/">Retourner sur la page d’accueil</Link>
     </div>
